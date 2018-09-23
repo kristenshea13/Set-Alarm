b45e7f1f766765c6786ef5b2190ef2b1026114bf
Kristen Shea <kstar61@live.com>
2018-09-23 15:24:48 -0500

    program to take user-entered time, convert to 24 hour time, add user-entered number of hours, and return the new time to set the *alarm* for.

--- Set Alarm/Program.cs
+++ Set Alarm/Program.cs
@@ -6,7 +6,46 @@ namespace Set_Alarm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //explain program
+            Console.WriteLine("Set an alarm!");
+
+            //ask user for what time it is, specify format
+            Console.Write("What time is it? (ex. 1:38 pm) ");
+
+            //convert to datetime, assign variable
+            DateTime currentTime = DateTime.Parse(Console.ReadLine());
+
+            //print to console as military time
+            Console.WriteLine($"Current time per 24 hour clock: {currentTime:HH:mm}");
+
+            //prompt for how many hours from now they want the alarm to go off
+            Console.Write("How many hours from now would you like to set an alarm for? ");
+
+            //convert to int and assign variable
+            int hours = int.Parse(Console.ReadLine());
+
+
+            //take method and convert to datetime variable so it can be formatted when printing to console
+            DateTime alarm = AlarmSet(currentTime, hours);
+
+            Console.WriteLine($"Your alarm is set for: {alarm:M/dd/yyyy HH:mm:ss}");
+
+            Console.ReadLine();
+
         }
+
+        /// <summary>
+        /// adds number of hours to time entered
+        /// </summary>
+        /// <param name="currentTime"></param>
+        /// <param name="hours"></param>
+        /// <returns>time it will be in number of hours user entered</returns>
+        private static DateTime AlarmSet(DateTime currentTime, int hours)
+        {
+            return currentTime.AddHours(hours);
+        }
+        
+
+
     }
 }
